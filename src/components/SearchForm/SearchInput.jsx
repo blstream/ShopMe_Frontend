5c342dc7c28d1db377f9aa0a09e7fa637a2f9cff
spieszku <spiechowicz.a@gmail.com>
2018-03-15 23:54:45 +0100

    PRODPFIP-97 Add checking if written phrase has more than one character

--- src/components/SearchForm/SearchInput.jsx
+++ src/components/SearchForm/SearchInput.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 
 export default class SearchInput extends React.Component {
+    constructor () {
+        super ();
+        this.state = {
+            searchPhrase: '',
+            validPhrase: true
+        }
+    }
     handleBlur = input => { 
         const searchPhrase = input.target.value.trim();
         const cleanedSearchPhrase = searchPhrase.replace(/[!@#$^&*()=+-_;:'"<>,.?/{}|`~[\]\\]/g, '');
-        console.log(cleanedSearchPhrase);
+        const validPhrase = cleanedSearchPhrase.length <= 1  ? false : true;
+        this.setState({searchPhrase: cleanedSearchPhrase, validPhrase: validPhrase });
     };
     render () {
         return (
-            <input 
-                type='text'
-                id='offer-search-input'
-                placeholder='Wpisz szukaną usługę'
-                name='searchPhrase'
-                onBlur={this.handleBlur}
-            />
+            <div>
+                <input 
+                    type='text'
+                    id='offer-search-input'
+                    placeholder='Wpisz szukaną usługę'
+                    name='searchPhrase'
+                    onBlur={this.handleBlur}
+                />
+                <p>{ this.state.validPhrase ? '' : 'Wpisana fraza jest za krótka' } </p>
+            </div>
         );
     }
 }
