2b89e00d4d897e30cfe681357d0b9f2f754a34ab
Łukasz Kowalski <lkowalski9090@gmail.com>
2018-03-16 18:38:44 +0100

    Code review fix

--- src/components/search/search-component.jsx
+++ src/components/search/search-component.jsx
@@ -3,32 +3,12 @@ import SearchInput from './search-input';
 import './search-component.css';
 
 export default class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      services: [
-        {
-          name: 'Pranie i sprzątanie',
-          price: '10zł',
-          date: '10.10.2010'
-        }, {
-          name: 'Sprzątanie',
-          price: '20zł',
-          date: '20.10.2015'
-        }, {
-          name: 'Pranie',
-          price: '30zł',
-          date: '05.10.2010'
-        }
-      ]
-    };
-  }
 
   render() {
     return (
       <div>
         <p className="logo">ShopMe</p>
-        <SearchInput services={this.state.services}/>
+        <SearchInput/>
       </div>
     );
   }
