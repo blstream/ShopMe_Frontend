75aaa97c3ea94697a440843b30eb813cfdf6101b
Łukasz Kowalski <lkowalski9090@gmail.com>
2018-03-16 19:27:52 +0100

    eslint fix

--- src/components/search/search-component.jsx
+++ src/components/search/search-component.jsx
@@ -3,12 +3,17 @@ import SearchInput from './search-input';
 import './search-component.css';
 
 export default class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
+    };
+  }
   render() {
     return (
       <div>
         <p className="logo">ShopMe</p>
-        <SearchInput/>
+        <SearchInput />
       </div>
     );
   }
