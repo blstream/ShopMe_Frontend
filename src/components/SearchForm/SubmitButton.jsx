e612c7147cd1cdd585968c24836eaae65ebd9df8
spieszku <spiechowicz.a@gmail.com>
2018-03-16 16:54:34 +0100

    PRODPFIP-97 Add translation of Offer search form

--- src/components/SearchForm/SubmitButton.jsx
+++ src/components/SearchForm/SubmitButton.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { translate } from 'react-i18next';
 
-export default class SubmitButton extends React.Component {
+class SubmitButton extends React.Component {
     handleSubmit = submit => { 
         submit.preventDefault();
     };
     render () {
+        const { t } = this.props;
         return (
             <input 
                 type='submit'
                 name='searchSubmit'
-                value='Szukaj'
+                value={t('components.searchForm.button')}
                 onClick={this.handleSubmit}
             />
         );
     }
 }
+
+export { SubmitButton };
+export default translate()(SubmitButton);
