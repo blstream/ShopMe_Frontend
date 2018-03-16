ee13f8520fa790ab997c178129743f1a0b9208f9
spieszku <spiechowicz.a@gmail.com>
2018-03-16 16:54:34 +0100

    PRODPFIP-97 Add translation of Offer search form

--- src/components/SearchForm/SearchInput.jsx
+++ src/components/SearchForm/SearchInput.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { translate } from 'react-i18next';
 
-export default class SearchInput extends React.Component {
+class SearchInput extends React.Component {
     constructor () {
         super ();
         this.state = {
@@ -10,22 +11,27 @@ export default class SearchInput extends React.Component {
     }
     handleBlur = input => { 
         const searchPhrase = input.target.value.trim();
-        const cleanedSearchPhrase = searchPhrase.replace(/[!@#$^&*()=+-_;:'"<>,.?/{}|`~[\]\\]/g, '');
+        const cleanedSearchPhrase = searchPhrase.replace(/[!@#$%^&*()=+-_;:'"<>,.?/{}|`~[\]\\]/g, '');
         const validPhrase = cleanedSearchPhrase.length <= 1  ? false : true;
+        console.log(cleanedSearchPhrase);
         this.setState({searchPhrase: cleanedSearchPhrase, validPhrase: validPhrase });
     };
     render () {
+        const { t } = this.props;        
         return (
             <div>
                 <input 
                     type='text'
                     id='offer-search-input'
-                    placeholder='Wpisz szukaną usługę'
+                    placeholder={t('components.searchForm.input')}
                     name='searchPhrase'
                     onBlur={this.handleBlur}
                 />
-                <p>{ this.state.validPhrase ? '' : 'Wpisana fraza jest za krótka' } </p>
+                <p>{ this.state.validPhrase ? '' : t('components.searchForm.lengthError') } </p>
             </div>
         );
     }
 }
+
+export { SearchInput };
+export default translate()(SearchInput);
