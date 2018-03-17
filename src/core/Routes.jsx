aa1b2d5deb9ae279ba66653e3d67e9a2cdcc5086
Mia <malgorzata.emilia.cieslewicz@gmail.com>
2018-03-17 07:41:22 +0100

    PRODPFIP-40 Prepare form HTML structure

--- src/core/Routes.jsx
+++ src/core/Routes.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import ScreensExamplesHello from 'screens/Examples/Hello';
 import ScreensExamplesFetch from 'screens/Examples/Fetch';
+import ScreensAddForm from 'screens/Add/Form/Form';
 
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route path="/examples/hello" component={ScreensExamplesHello} />
       <Route path="/examples/fetch" component={ScreensExamplesFetch} />
+      <Route path="/add/form" component={ScreensAddForm} />
     </Switch>
   </BrowserRouter>
 );
