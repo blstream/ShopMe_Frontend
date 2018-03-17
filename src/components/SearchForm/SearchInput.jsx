56a64275bf8aef46188067f86771546456c3c097
spieszku <spiechowicz.a@gmail.com>
2018-03-17 20:03:39 +0100

    PRODPFIP-97 Refactoring of SearchInput component:

--- src/components/SearchForm/SearchInput.jsx
+++ src/components/SearchForm/SearchInput.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { translate } from 'react-i18next';
 
 class SearchInput extends React.Component {
-    constructor () {
-        super ();
+    constructor (props) {
+        super (props);
         this.state = {
             searchPhrase: '',
             validPhrase: true
@@ -11,9 +11,8 @@ class SearchInput extends React.Component {
     }
     handleBlur = input => {
         const searchPhrase = input.target.value.trim();
-        const cleanedSearchPhrase = searchPhrase.replace(/[!@#$%^&*()=+-_;:'"<>,.?/{}|`~[\]\\]/g, '');
-        const validPhrase = cleanedSearchPhrase.length <= 1  ? false : true;
-        console.log(cleanedSearchPhrase);
+        const cleanedSearchPhrase = searchPhrase.replace(/[!@#$%^&*()=+\-_;:'"<>,.?/{}|`~[\]\\]/g, '');
+        const validPhrase = cleanedSearchPhrase.length > 1;
         this.setState({searchPhrase: cleanedSearchPhrase, validPhrase: validPhrase });
     };
     render () {
