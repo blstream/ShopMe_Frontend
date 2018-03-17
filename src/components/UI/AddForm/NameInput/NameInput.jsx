b8e21b1e974d8b515859044550d1146acb6cb32e
Patryk Szmurło <patryk_szm1@outlook.com>
2018-03-17 21:00:40 +0100

    PRODPFIP-90 Added some validation and EN translations

--- src/components/UI/AddForm/NameInput/NameInput.jsx
+++ src/components/UI/AddForm/NameInput/NameInput.jsx
@@ -1,38 +1,60 @@
 import React, { Component } from 'react';
-// import { translate } from 'react-i18next';
+import { translate } from 'react-i18next';
 
 class AddFormNameInput extends Component {
   constructor(props) {
     super(props);
-    this.state = { name: '' };
+    this.state = {
+      value: '',
+      errorMessage: '',
+    };
   }
 
-  handleNameChange = (event) => {
-      this.setState({name: event.target.value});
-  };
+  checkValidity = (event) => {
+    const { t } = this.props;
+    const isValid = true;
+
+    if (event.target.value.trim() === '' && isValid) {
+      this.setState({ errorMessage: t('components.UI.AddForm.NameInput.errorEmptyField') });
+      return false;
+    }
+    if (event.target.value.length > 20) {
+      this.setState({ errorMessage: t('components.UI.AddForm.NameInput.errorMaxLength') });
+    }
+    if (event.target.value.length < 3) {
+      this.setState({ errorMessage: t('components.UI.AddForm.NameInput.errorMinLength') });
+    }
+    const pattern = /^[a-zA-Z]+$/;
+    if (!pattern.test(event.target.value)) {
+      this.setState({ errorMessage: t('components.UI.AddForm.NameInput.errorOnlyAlpha') });
+    }
+    return isValid;
+  }
 
   render() {
+    const { t } = this.props;
     return (
       <div className="AddFormName">
         <label htmlFor="name">
-          Imię *
-          <input
-            className="AddFormNameInput"
-            type="text"
-            name={this.props.name}
-            value={this.state.name}
-            onChange={this.handleNameChange}
-          />
+          {t('components.UI.AddForm.NameInput.name')}
+          <div>
+            <input
+              className="AddFormNameInput"
+              type="text"
+              name={this.props.name}
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value, errorMessage: '' })}
+              onBlur={this.checkValidity}
+            />
+          </div>
         </label>
-        {/* {this.renderNameError()} */}
+        <div className="AddFormNameErrorMessage">
+          {this.state.errorMessage}
+        </div>
       </div>
     );
   }
-//   renderNameError = () =>{
-
-//   }
 }
 
-// export { AddFormNameInput };
-// export default translate()(AddFormNameInput);
-export default AddFormNameInput;
+export { AddFormNameInput };
+export default translate()(AddFormNameInput);
