95b5621c344d2633d026d831effead942b5b983b
Łukasz Kowalski <lkowalski9090@gmail.com>
2018-03-17 11:43:31 +0100

    file names fix

--- src/core/Routes.jsx
+++ src/core/Routes.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import ScreensExamplesHello from 'screens/Examples/Hello';
 import ScreensExamplesFetch from 'screens/Examples/Fetch';
-import Search from 'components/search/search-component';
+import ScreensSearch from 'screens/Search/Search';
 
 export default() => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Search}/>
-      <Route path="/examples/hello" component={ScreensExamplesHello}/>
-      <Route path="/examples/fetch" component={ScreensExamplesFetch}/>
+      <Route exact path="/" component={ScreensSearch} />
+      <Route path="/examples/hello" component={ScreensExamplesHello} />
+      <Route path="/examples/fetch" component={ScreensExamplesFetch} />
     </Switch>
   </BrowserRouter>
 );
