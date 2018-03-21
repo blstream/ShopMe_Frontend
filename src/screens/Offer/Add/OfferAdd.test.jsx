01e20b29c89cb33a8194bbffed27c62ea4233b5d
Radoslaw Kochaniuk <rkochaniuk@gmail.com>
2018-03-21 16:05:32 +0100

    default test

--- src/screens/Offer/Add/OfferAdd.test.jsx
+++ src/screens/Offer/Add/OfferAdd.test.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 import ScreenOfferAdd from './OfferAdd';
 
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-
-describe('<ScreenOfferAdd />', () => {
-  it('renders 1 <ScreenOfferAdd /> component', () => {
-    const component = shallow(<ScreenOfferAdd />);
-    expect(component).toHaveLength(1);
+describe('ScreenOfferAdd component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ScreenOfferAdd />, div);
   });
 });
