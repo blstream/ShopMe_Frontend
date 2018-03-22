a9abf7b1c52b3404cb8c7aef19346393ed9833d0
Agata Zimoch <kierzek.agata@gmail.com>
2018-03-22 21:25:50 +0100

    Connect search componetsand support for no results

--- src/core/Routes.jsx
+++ src/core/Routes.jsx
@@ -3,14 +3,12 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import ScreensSearch from 'screens/Search/Search';
 import ScreensAddForm from 'screens/Add/Form/Form';
-import ScreensServicesResults from 'screens/Services/Results';
 
 export default() => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={ScreensSearch} />
       <Route path="/add/form" component={ScreensAddForm} />
-      <Route path="/searchresults" component={ScreensServicesResults} />
     </Switch>
   </BrowserRouter>
 );
