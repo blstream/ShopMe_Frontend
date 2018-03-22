ca766b39c12148b407319b25f7ede2696c4efa8f
Radoslaw Kochaniuk <rkochaniuk@gmail.com>
2018-03-22 12:33:59 +0100

    PRODPFIP-83 --added href property for Link component  --classNames now rely on BEM Methodology  --constant font not defined yet

--- src/screens/Offer/Add/OfferAdd.test.jsx
+++ src/screens/Offer/Add/OfferAdd.test.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ScreenOfferAdd from './OfferAdd';
+import { ScreenOfferAdd } from './OfferAdd';
 
-describe('ScreenOfferAdd component', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<ScreenOfferAdd />, div);
-  });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ScreenOfferAdd t={key => key} />, div);
 });
