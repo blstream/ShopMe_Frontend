08647ce36b7b08177aeb17d14af5aa45e1ddccf3
Łukasz Kowalski <lkowalski9090@gmail.com>
2018-03-24 17:55:30 +0100

    podłączenie linku do logo

--- src/components/UI/Logo.jsx
+++ src/components/UI/Logo.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Logo = () => (
-  <div>
-    <p className="logo">ShopMe</p>
+  <div className="logoContainer">
+    <Link href="/" to="/">
+      <p className="logoContainer__logo">ShopMe</p>
+    </Link>
   </div>
 );
 
