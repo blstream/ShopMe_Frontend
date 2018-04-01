d98c94fea1158621ed4bddffeaac4bb771e9e7dc
Mia <malgorzata.emilia.cieslewicz@gmail.com>
2018-04-01 20:33:05 +0200

    PRODPFIP-158 Add logIn button to homepage

--- src/screens/Search/Search.jsx
+++ src/screens/Search/Search.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchInput from 'components/Search/SearchInput';
 import Logo from 'components/UI/Logo';
 import OfferAdd from 'components/UI/Add/OfferAdd';
+import LoginButton from 'components/UI/LoginButton/LoginButton';
 import 'components/Search/Search.css';
 
 const Search = () => (
@@ -9,9 +10,10 @@ const Search = () => (
     <header>
       <Logo />
       <nav />
-      <OfferAdd />
+      <LoginButton />
     </header>
     <content>
+      <OfferAdd />
       <SearchInput />
     </content>
     <footer />
