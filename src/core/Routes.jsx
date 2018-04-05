5e789043ca998abb3c495eb52848176fa267ca80
Radoslaw Kochaniuk <rkochaniuk@gmail.com>
2018-04-05 12:05:37 +0200

    added SuccessAdd screen

--- src/core/Routes.jsx
+++ src/core/Routes.jsx
@@ -4,11 +4,13 @@ import ScreensSearch from 'screens/Search/Search';
 import ScreensAddForm from 'screens/Add/Form/Form';
 import ScreensOfferDetails from 'screens/OfferDetails/OfferDetails';
 import ScreensLogin from 'screens/Login/Login';
+import ScreenSuccessAdd from 'screens/Add/SuccessAdd/SuccessAdd';
 
 export default() => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={ScreensSearch} />
+      <Route exact path="/add/form/success" component={ScreenSuccessAdd} />
       <Route path="/add/form" component={ScreensAddForm} />
       <Route path="/offer/:offerId" component={ScreensOfferDetails} />
       <Route path="/login" component={ScreensLogin} />
