44260c2bd378b34f505290d8d1a1118f347a5dff
Radoslaw Kochaniuk <rkochaniuk@gmail.com>
2018-04-05 13:41:29 +0200

    circleci: build --test fix

--- src/components/Add/SuccessMessage/SuccessMessage.test.jsx
+++ src/components/Add/SuccessMessage/SuccessMessage.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { SuccessMessage } from './SuccessMessage';
+import SuccessMessage from './SuccessMessage';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
