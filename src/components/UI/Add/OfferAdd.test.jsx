f1932dc33df9b2f29b864e060d50418d648ed02c
spieszku <spiechowicz.a@gmail.com>
2018-04-09 22:52:03 +0200

    Change structure of links according to W3C

--- src/components/UI/Add/OfferAdd.test.jsx
+++ src/components/UI/Add/OfferAdd.test.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-import { ScreenOfferAdd } from './OfferAdd';
+import { AddOfferLink } from './OfferAdd';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<ScreenOfferAdd t={key => key} />, div);
+  ReactDOM.render(<AddOfferLink t={key => key} />, div);
 });
