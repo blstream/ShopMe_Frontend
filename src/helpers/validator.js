c6280ae3a9681d4a9f5ddf445cffee7075398567
Malgorzata Cieslewicz <malgorzata.emilia.cieslewicz@gmail.com>
2018-04-11 17:25:57 +0200

    Change use of validator object in application

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -1,31 +1,37 @@
 const validator = {
+
   isRequired(required, value) {
     return (value.trim() === '' && required) ? 'components.UI.firstNameInput.errorEmptyField' : undefined;
   },
+
   hasMinLength(minLength, value) {
     return value.length < minLength ? 'components.UI.firstNameInput.errorMinLength' : undefined;
   },
+
   useOnlyAlpha(value) {
-    const pattern = /^[a-zA-Z]+$/;
+    const pattern = /^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]*$/;
     return !pattern.test(value) ? 'components.UI.firstNameInput.errorOnlyAlpha' : undefined;
   },
+
+  isChecked(checked) {
+    return (!checked) ? 'components.UI.firstNameInput.errorEmptyField' : undefined;
+  },
+
   validateNameInput(required, value) {
     return validator.isRequired(required, value) ||
-      validator.hasMinLength(3, value) ||
+      validator.hasMinLength(2, value) ||
       validator.useOnlyAlpha(value) ||
       undefined;
   },
+
   validateTextInput(required, value) {
     return validator.isRequired(required, value) ||
       undefined;
   },
 
-  validatePersonalDataConfirmCheckbox(checked) {
-    if (checked === false) {
-      this.errorMessage = 'components.UI.firstNameInput.errorEmptyField';
-      return this.errorMessage;
-    }
-    return false;
+  validateCheckbox(checked) {
+    return validator.isChecked(checked) ||
+      undefined;
   },
 };
 
