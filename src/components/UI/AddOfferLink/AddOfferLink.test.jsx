5bca46f4d0fa27bb23b05cb0d5ad89f7bb0c176b
spieszku <spiechowicz.a@gmail.com>
2018-04-12 06:52:36 +0200

    Fix conflicts

--- src/components/UI/AddOfferLink/AddOfferLink.test.jsx
+++ src/components/UI/AddOfferLink/AddOfferLink.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AddOfferLink } from './AddOfferLink';
 import { MemoryRouter } from 'react-router-dom';
 
-import { AddOfferLink } from './AddOfferLink';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
