db8f5b4c5dc86f75b563e04a0627301a63d7f03d
Łukasz Kowalski <lkowalski9090@gmail.com>
2018-04-19 12:56:33 +0200

    redirect from Login to Login screen

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import { translate } from 'react-i18next';
+import { Redirect } from 'react-router';
 import LoginForm from 'components/Login/LoginForm/LoginForm';
 import SignupForm from 'components/Login/SignupForm/SignupForm';
 import Layout from 'core/Layout';
 
-const SreensLogin = () => (
-  <Layout className="login-form__wrapper">
-    <LoginForm />
-    <SignupForm />
-  </Layout>
-);
+class ScreensLogin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loginFireRedirect: false,
+    };
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+  }
 
-export { SreensLogin };
-export default translate()(SreensLogin);
+  handleLoginSubmit(e) {
+    e.preventDefault();
+    document.cookie = 'userToken=test';
+    const isUserLogged = !!document.cookie.replace(/(?:(?:^|.*;\s*)userToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    if (isUserLogged) {
+      this.setState({ loginFireRedirect: true });
+    }
+  }
+
+  render() {
+    return (
+      <Layout className="login-form__wrapper">
+        {this.state.loginFireRedirect && (
+        <Redirect to="/" />
+        )}
+        <LoginForm
+          handleSubmit={this.handleLoginSubmit}
+          fireRedirect={this.state.loginFireRedirect}
+        />
+        <SignupForm />
+      </Layout>
+    );
+  }
+}
+
+export { ScreensLogin };
+export default translate()(ScreensLogin);
