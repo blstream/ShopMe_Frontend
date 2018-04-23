a3813020684220c3d0d4501b42c961568dd90422
spieszku <spiechowicz.a@gmail.com>
2018-04-23 20:22:39 +0200

    Remove Search/SearchInput component and move methods to Screens/Search

--- src/screens/Search/Search.jsx
+++ src/screens/Search/Search.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+<<<<<<< HEAD
 import SearchInput from 'components/Search/SearchInput';
+=======
+import Layout from 'core/Layout';
+import SearchForm from 'components/Search/SearchForm/SearchForm';
+import FoundSearchResults from 'components/Search/SearchResults/FoundSearchResults/FoundSearchResults';
+import NoSearchResults from 'components/Search/SearchResults/NoSearchResults/NoSearchResults';
+
+>>>>>>> Remove Search/SearchInput component and move methods to Screens/Search
 
 export default class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       services: undefined,
+      foundServices: [],
+      notFoundServices: null,
+      searchPhrase: null,
+      paginationData: {},
     };
     this.getData = this.getData.bind(this);
+    this.updateFoundServices = this.updateFoundServices.bind(this);
+    this.updateSearchPhrase = this.updateSearchPhrase.bind(this);
+    this.updatePaginationData = this.updatePaginationData.bind(this);
   }
 
   getData(args) {
@@ -15,20 +30,54 @@ export default class Search extends React.Component {
       .then(response => response.json())
       .then((services) => { this.setState({ services }); });
   }
+  updateFoundServices(foundServices) {
+    if (foundServices.content) {
+      this.setState({ foundServices: foundServices.content, notFoundServices: false });
+    } else {
+      this.setState({ notFoundServices: true });
+    }
+  }
+
+  updatePaginationData(paginationData) {
+    this.setState({ paginationData });
+  }
+
+  updateSearchPhrase(searchPhrase) {
+    this.setState({ searchPhrase });
+  }
 
   render() {
     const searchQueryValue = new URLSearchParams(document.location.search.substring(1));
     const searchQuery = searchQueryValue.get('title');
+    let results;
+    if (this.state.notFoundServices === false) {
+      results = (<FoundSearchResults
+        services={this.state.foundServices}
+        updateFoundServices={this.updateFoundServices}
+        searchPhrase={this.state.searchPhrase}
+        paginationData={this.state.paginationData}
+      />);
+    } else if (this.state.notFoundServices === true) {
+      results = (<NoSearchResults />);
+    } else {
+      results = ('');
+    }
     return (
 <<<<<<< HEAD
       <SearchInput onSubmit={this.getData} services={this.state.services} />
 =======
       <Layout>
-        <SearchInput
-          onSubmit={this.getData}
-          services={this.state.services}
-          searchQuery={searchQuery}
-        />
+        <div className="search">
+          <SearchForm
+            updateFoundServices={this.updateFoundServices}
+            updateSearchPhrase={this.updateSearchPhrase}
+            updatePaginationData={this.updatePaginationData}
+            onSubmit={this.getData}
+            services={this.state.services}
+            searchQuery={searchQuery}
+          />
+          {results}
+        </div>
       </Layout>
 >>>>>>> Pass values from url to SearchInput component
     );
