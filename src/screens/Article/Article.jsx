73286fc821051588eaa28a8a899b201c9bb508b7
Agata Zimoch <kierzek.agata@gmail.com>
2018-04-24 21:54:14 +0200

    Added folder assets in public, created components MarkdownArticles in UI and Article in screens

---
+++ src/screens/Article/Article.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { translate } from 'react-i18next';
+import Layout from 'core/Layout';
+import MarkdownArticle from 'components/UI/MarkdownArticle/MarkdownArticle';
+
+class Article extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: '',
+    };
+
+    fetch(`/assets/articles/pl/${props.match.params.article}.md`)
+      .then(response => response.text())
+      .then((article) => {
+        this.setState({ content: article });
+      });
+  }
+
+  render() {
+    return (
+      <Layout className="article">
+        <MarkdownArticle
+          source={this.state.content}
+        />
+      </Layout>
+    );
+  }
+}
+
+
+export { Article };
+export default translate()(Article);
