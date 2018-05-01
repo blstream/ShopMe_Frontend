c3883e73887c0ee92776047ec5be58d39a2177b4
Agata Zimoch <kierzek.agata@gmail.com>
2018-05-01 22:58:12 +0200

    Added translation to Terms

--- src/components/App/Terms/Terms.jsx
+++ src/components/App/Terms/Terms.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { translate } from 'react-i18next';
+
 import './Terms.css';
 
 class Terms extends React.Component {
@@ -9,29 +11,30 @@ class Terms extends React.Component {
   }
 
   render() {
+    const { t } = this.props;
     return (
       <div className="terms">
-        <h2 className="terms__caption">Ty określasz warynki</h2>
+        <h2 className="terms__caption">{t('components.UI.terms.header')}</h2>
         <div className="term__column-wrapper">
           <div className="terms__column">
-            <h3 className="terms__column--title">Budżetowe</h3>
-            <div className="terms__column--contents">Filtruj wyniki wyszukiwania aby dopasować je do preferencji finansowych</div>
+            <h3 className="terms__column--title">{t('components.UI.terms.budgetary.title')}</h3>
+            <div className="terms__column--contents">{t('components.UI.terms.budgetary.content')}</div>
           </div>
           <div className="terms__column">
-            <h3 className="terms__column--title">Czasowe</h3>
-            <div className="terms__column--contents">Wyszukuj usługi dopasowane do Twoich ram czasowych</div>
+            <h3 className="terms__column--title">{t('components.UI.terms.time.title')}</h3>
+            <div className="terms__column--contents">{t('components.UI.terms.time.content')}</div>
           </div>
           <div className="terms__column">
-            <h3 className="terms__column--title">Formy płatności</h3>
-            <div className="terms__column--contents">
-              Wybierz jedną z proponowancy przez nas form płatności dla pełnego bezpieczeństwa
-            </div>
+            <h3 className="terms__column--title">{t('components.UI.terms.payment.title')}</h3>
+            <div className="terms__column--contents">{t('components.UI.terms.payment.content')}</div>
           </div>
         </div>
-        <h2 className="terms__caption">My gwarantujemy bezpeczeństwo i satysfakcję</h2>
+        <h2 className="terms__caption">{t('components.UI.terms.footer')}</h2>
       </div>
     );
   }
 }
 
-export default Terms;
+export { Terms };
+export default translate()(Terms);
+
