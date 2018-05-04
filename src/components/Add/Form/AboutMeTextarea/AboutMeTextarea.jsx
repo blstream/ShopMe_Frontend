5cdae6d735956e7eb292c9654f1dd6c788a3120d
Radoslaw Kochaniuk <rkochaniuk@gmail.com>
2018-05-04 14:35:32 +0200

    few components related with Add/Form has been moved to desirable location

---
+++ src/components/Add/Form/AboutMeTextarea/AboutMeTextarea.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { translate } from 'react-i18next';
+
+import './AboutMeTextarea.css';
+
+class AboutMeTextArea extends Component {
+  constructor(props) {
+    super(props);
+
+    this.checkValidity = this.checkValidity.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      value: '',
+      errorMessage: '',
+      isRequired: this.props.required,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.onValidate) {
+      this.props.doValidate(this.props.name, this.state.value, this.checkValidity());
+    }
+  }
+
+  handleChange(event) {
+    this.setState({ errorMessage: '' });
+    if (event.target.value.length <= 800) this.setState({ value: event.target.value });
+  }
+
+  checkValidity() {
+    const { t } = this.props;
+    const isValid = true;
+
+    if (this.state.isRequired && this.state.value.trim() === '') {
+      this.setState({ errorMessage: t('components.UI.aboutMeTextarea.errorEmptyField') });
+      return false;
+    }
+    return isValid;
+  }
+
+  render() {
+    const { t } = this.props;
+    return (
+      <label
+        className="add-form__label add-form__label--yellow"
+        htmlFor={this.props.name}
+      >
+        <div>
+          {t('components.UI.aboutMeTextarea.name')}
+        </div>
+        <textarea
+          className="add-form__input add-form__input--yellow add-form__input--L"
+          name={this.props.name}
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <div className="add-form__error-message">
+          {this.state.errorMessage}
+        </div>
+      </label>
+    );
+  }
+}
+
+export { AboutMeTextArea };
+export default translate('translations', { withRef: true })(AboutMeTextArea);
---
+++ src/components/Add/Form/PriceInput/PriceInput.jsx
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import { translate } from 'react-i18next';
+
+import './PriceInput.css';
+
+class PriceInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      errorMessage: '',
+    };
+    this.checkValidity = this.checkValidity.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.onValidate) {
+      this.props.doValidate(this.props.name, this.state.value, this.checkValidity());
+    }
+  }
+
+  checkValidity() {
+    const { required, disabled, t } = this.props;
+    const isValid = true;
+
+    if (this.state.value.trim() === '' && required && !disabled) {
+      this.setState({ errorMessage: t('components.UI.priceInput.errorEmptyField') });
+      return false;
+    }
+
+    this.setState({ errorMessage: '' });
+    return isValid;
+  }
+
+  handleChange(e) {
+    const { value } = e.target;
+    const price = /^([0-9]*)(,)?([0-9]{0,2})?$/;
+
+    if (price.test(value)) this.setState({ value });
+    if (value.trim() !== '') this.activateNextField();
+    if (value.trim() === '') this.deactivateNextFields();
+  }
+
+  activateNextField() {
+    if (this.props.name === 'offerExtendedPrice') {
+      this.props.onChange('offerExtendedRequired', true);
+    }
+    if (this.props.name === 'offerExtraPrice') {
+      this.props.onChange('offerExtraRequired', true);
+    }
+  }
+
+  deactivateNextFields() {
+    if (this.props.name === 'offerExtendedPrice') {
+      this.props.onChange('offerExtendedRequired', false);
+    }
+    if (this.props.name === 'offerExtraPrice') {
+      this.props.onChange('offerExtraRequired', false);
+    }
+  }
+
+  handleKeyUp(e) {
+    let { value } = e.target;
+    const onlyComma = /^,/;
+
+    if (onlyComma.test(value)) value = '0,';
+
+    this.setState({ value });
+  }
+
+  handleBlur(e) {
+    let { value } = e.target;
+    const state = this.state.value;
+
+    if (state) value = parseFloat(value.replace(',', '.')).toFixed(2).replace('.', ',');
+    this.setState({ value });
+  }
+
+  render() {
+    const { t } = this.props;
+    return (
+      <label htmlFor={this.props.name} className="add-form__label">
+        <div className="add-form__label--hidden">{t('components.UI.PriceInput.label')}</div>
+        <input
+          type="text"
+          className={this.props.disabled
+            ? 'add-form__input add-form__input--XS add-form__input--disabled'
+            : 'add-form__input add-form__input--XS'
+          }
+          name={this.props.name}
+          value={this.state.value}
+          disabled={this.props.disabled}
+          required={this.props.required}
+          onChange={this.handleChange}
+          onKeyUp={this.handleKeyUp}
+          onBlur={this.handleBlur}
+        />
+        <div className="add-form__error-message">{this.state.errorMessage}</div>
+        <div className="add-form__input-currency">{t('components.UI.priceInput.currency')}</div>
+      </label>
+    );
+  }
+}
+
+export { PriceInput };
+export default translate('translations', { withRef: true })(PriceInput);
