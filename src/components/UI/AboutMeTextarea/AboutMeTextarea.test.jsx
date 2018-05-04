424e63f57ad8e83e851ba64efc2a7d391d878334
Radoslaw Kochaniuk <rkochaniuk@gmail.com>
2018-05-04 14:34:42 +0200

    few components related with Add/Form has been moved to desirable location

--- src/components/UI/AboutMeTextarea/AboutMeTextarea.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AboutMeTextArea from './AboutMeTextarea';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AboutMeTextArea />, div);
-});
