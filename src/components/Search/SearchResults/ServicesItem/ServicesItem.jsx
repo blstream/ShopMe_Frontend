62facf4e1e30c6860057a131983d9d5bd557a5a3
Patryk Szmurło <patryk_szm1@outlook.com>
2018-05-13 21:33:45 +0200

    Add category to search results presentation

--- src/components/Search/SearchResults/ServicesItem/ServicesItem.jsx
+++ src/components/Search/SearchResults/ServicesItem/ServicesItem.jsx
@@ -14,6 +14,7 @@ const ServicesItem = (props) => {
       >
         {((props.page - 1) * 10) + props.index + 1}. {props.value.title}
       </Link>
+      <span className="services-item services-item__price">{props.value.category.name}</span>
       <span className="services-item services-item__price">{props.value.basePrice} zł</span>
       <span className="services-item services-item__date">{date}</span>
     </li>
