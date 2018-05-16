f61f7be1fa3cb1b9274819ca1fe093a62c4685d1
Malgorzata Cieslewicz <malgorzata.emilia.cieslewicz@gmail.com>
2018-05-17 00:07:25 +0200

    Add mocks to tests

--- src/components/UI/GenericSelect/GenericSelect.test.jsx
+++ src/components/UI/GenericSelect/GenericSelect.test.jsx
@@ -4,5 +4,5 @@ import GenericSelect from './GenericSelect';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<GenericSelect />, div);
+  ReactDOM.render(<GenericSelect selectData={[1, 2, 3]} />, div);
 });
