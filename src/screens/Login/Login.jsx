6d92487f22c548e35e30f1e0a1c8eab8ae874460
Łukasz Kowalski <lkowalski9090@gmail.com>
2018-05-17 14:35:28 +0200

    login implementation

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -6,19 +6,39 @@ import LoginForm from 'components/Login/LoginForm/LoginForm';
 class LoginScreen extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       loginFireRedirect: false,
+      response: {
+        userId: undefined,
+        email: undefined,
+        name: undefined,
+        surname: undefined,
+        roles: undefined,
+        expirationDate: undefined,
+        jwt: undefined,
+        id: undefined,
+        message: undefined,
+      },
     };
     this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.logUser = this.logUser.bind(this);
   }
 
-  handleLoginSubmit(e) {
-    e.preventDefault();
-    localStorage.setItem('userToken', 'test');
-    const user = localStorage.getItem('userToken');
-    const isUserLogged = !!localStorage.getItem('userToken');
-    this.props.setUserToken(user);
+  logUser(data) {
+    const { http } = this.props;
+    return http.post('/api/users/login', data)
+      .then(response => this.setState({ response }));
+  }
+
+  handleLoginSubmit() {
+    if (this.state.response.jwt) localStorage.setItem('userToken', this.state.response.jwt);
+    if (this.state.response.name) localStorage.setItem('userName', this.state.response.name);
+    if (this.state.response.surname) localStorage.setItem('userSurname', this.state.response.surname);
+    const token = localStorage.getItem('userToken');
+    const name = localStorage.getItem('userName');
+    const surname = localStorage.getItem('userSurname');
+    const isUserLogged = !!localStorage.getItem('userName');
+    this.props.setUser(token, name, surname);
     if (isUserLogged) this.setState({ loginFireRedirect: true });
   }
 
@@ -26,7 +46,10 @@ class LoginScreen extends React.Component {
     return (
       <div className="login-form__wrapper">
         {this.state.loginFireRedirect && <Redirect to="/" />}
-        <LoginForm handleSubmit={this.handleLoginSubmit} />
+        <LoginForm
+          handleSubmit={this.handleLoginSubmit}
+          logUser={this.logUser}
+        />
       </div>
     );
   }
