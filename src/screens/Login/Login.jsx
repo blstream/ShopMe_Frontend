3985cfcb24218fcf54dd3ff3d4003b9cccba9ee0
Łukasz Kowalski <lkowalski9090@gmail.com>
2018-05-20 13:52:38 +0200

    response in state removed

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -8,30 +8,19 @@ class LoginScreen extends React.Component {
     super(props);
     this.state = {
       loginFireRedirect: false,
-      response: {
-        userId: undefined,
-        email: undefined,
-        name: undefined,
-        surname: undefined,
-        roles: undefined,
-        expirationDate: undefined,
-        jwt: undefined,
-        id: undefined,
-        message: undefined,
-      },
     };
     this.logUser = this.logUser.bind(this);
   }
 
-  setUser() {
-    if (this.state.response.jwt) localStorage.setItem('userToken', this.state.response.jwt);
-    if (this.state.response.name) localStorage.setItem('userName', this.state.response.name);
-    if (this.state.response.surname) localStorage.setItem('userSurname', this.state.response.surname);
+  setUser(response) {
+    if (response.jwt) localStorage.setItem('userToken', response.jwt);
+    if (response.name) localStorage.setItem('userName', response.name);
+    if (response.surname) localStorage.setItem('userSurname', response.surname);
     const isUserLogged = !!localStorage.getItem('userName');
     this.props.setUser(
-      this.state.response.jwt,
-      this.state.response.name,
-      this.state.response.surname
+      response.jwt,
+      response.name,
+      response.surname
     );
     if (isUserLogged) this.setState({ loginFireRedirect: true });
   }
@@ -39,8 +28,7 @@ class LoginScreen extends React.Component {
   logUser(data) {
     const { http } = this.props;
     return http.post('/api/users/login', data)
-      .then(response => this.setState({ response }))
-      .then(() => this.setUser());
+      .then(response => this.setUser(response));
   }
 
   render() {
