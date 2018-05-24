5720e2bb18498abd08ea87ff9e45df044d6c57f1
spieszku <spiechowicz.a@gmail.com>
2018-05-24 22:11:54 +0200

    Add search results styles

--- src/components/Search/SearchResults/ServicesItem/ServicesItem.jsx
+++ src/components/Search/SearchResults/ServicesItem/ServicesItem.jsx
@@ -8,6 +8,7 @@ const ServicesItem = (props) => {
   const date = myDate.toLocaleDateString();
   return (
     <li className="services-item">
+<<<<<<< HEAD
       <Link
         className="services-item__title"
         to={`/offer/${props.value.id}`}
@@ -17,6 +18,30 @@ const ServicesItem = (props) => {
       <span className="services-item__span">{props.t(`components.UI.categorySelect.categoryOptions.${props.value.category}`)}</span>
       <span className="services-item__span">{props.value.basePrice} {props.t('components.foundSearchResults.currency')}</span>
       <span className="services-item__span">{date}</span>
+=======
+      <div className="services-item__name services-item__element">
+        <Link
+          className="services-item__title"
+          to={`/offer/${props.value.id}`}
+        >
+          {props.value.title}
+        </Link>
+        <p>{props.value.user.city}</p>
+      </div>
+      <div className="services-item__element services-item__element--category">
+        <span className="services-item__span services-item__category">{props.t(`components.UI.categorySelect.categoryOptions.${props.value.category.name}`)}</span>
+      </div>
+      <div className="services-item__element">
+        <span className="services-item__price-prefix">{props.t('components.foundSearchResults.prefix')}</span>
+        <span className="services-item__span services-item__price">
+          {props.value.basePrice} {props.t('components.foundSearchResults.currency')}
+        </span>
+        <p>dostępne inne pakiety</p>
+      </div>
+      <div className="services-item__element">
+        <span className="services-item__date">{date}</span>
+      </div>
+>>>>>>> Add search results styles
     </li>
   );
 };
