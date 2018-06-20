45efbe0946c415426d9a227771e00f25ce90ccc8
Ardneh3 <32309180+Ardneh3@users.noreply.github.com>
2018-06-20 09:53:31 +0200

    enhancement/refs replacement (#265)
    * refs replacement
    
    * changes after review

--- src/components/Register/RegisterForm/InvoiceInputGroup/InvoiceInputGroup.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import InvoiceInputGroup from './InvoiceInputGroup';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<InvoiceInputGroup />, div);
-});
